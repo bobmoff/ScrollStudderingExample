947b1944eda4e292f385557af04fd0910cee5566
Fille Åström <fille@imgnry.com>
2014-03-15 16:03:15 +0100

    -

---
+++ ScrollStudderingLab/ScrollViewDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ScrollViewDelegate.h
+//  ScrollStudderingLab
+//
+//  Created by Fille Åström on 13/02/14.
+//  Copyright (c) 2014 Fille Åström. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScrollViewDelegate : NSObject
+
+@end
---
+++ ScrollStudderingLab/ScrollViewDelegate.m
@@ -0,0 +1,13 @@
+//
+//  ScrollViewDelegate.m
+//  ScrollStudderingLab
+//
+//  Created by Fille Åström on 13/02/14.
+//  Copyright (c) 2014 Fille Åström. All rights reserved.
+//
+
+#import "ScrollViewDelegate.h"
+
+@implementation ScrollViewDelegate
+
+@end
